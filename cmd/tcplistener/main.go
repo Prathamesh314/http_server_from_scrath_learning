34d04782a7798e4f18be00f7d87d262d13152fba
Prathamesh314 <kurvep18@gmail.com>
2025-09-21 18:51:28 +0530

    nothing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,50 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
 
 	"github.com/Prathamesh314/http_server_from_scrath_learning/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out_ch := make(chan string, 8)
-
-	go func() {
-		defer close(out_ch)
-		ans := ""
-		for {
-			buffer := make([]byte, 8)
-
-			n, err := f.Read(buffer)
-			if err == io.EOF {
-				break
-			}
-
-			if err != nil {
-				log.Fatalf("Error in reading into buffer: %s\n", err.Error())
-				break
-			}
-
-			data := buffer[:n]
-			if idx := strings.Index(string(data), "\n"); idx != -1 {
-				ans += string(data)[:idx]
-				data = data[idx+1:]
-				out_ch <- ans
-				ans = ""
-			}
-
-			ans += string(data)
-		}
-
-		if len(ans) != 0 {
-			out_ch <- ans
-		}
-	} ()
-	return out_ch
-}
 
 func main() {
 	// now you have to read data in same way
